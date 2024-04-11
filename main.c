dd5cd095773d64c16f9e022dbc18d6fb8f2bfebd
Meghana-30 <163978948+Meghana-30@users.noreply.github.com>
2024-04-11 21:45:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,58 +1,101 @@
+// Online C compiler to run C program online
+
 #include <stdio.h>
+    int choice;
+    int choice2;
+    int choice3;
+     char flavour[20][20]={"Vanilla","Strawberry","Chocolate","Pista","Butterscotch","Mango","Chocovanilla","Coffee","Caramel","Brownie","Coconut"};
+    int price_l[]={30,40,50,50,40,45,40,50,50,60};
+    int price_s[]={15,20,25,25,20,25,20,25,25,30};
+    int confirm;
 
-// Define a structure to represent ice cream flavors
-struct icecream {
-    char flavour[20];
-};
+void select()
+{
+      int num=1;
+  while (num!=0){
+  switch(choice){
+      case 1:
+      printf("We have 10 flavours for you\n");
+      for(int i=0;i<10;i++){
+        printf("%d. %s\n",i+1,flavour[i]);
+      }
+        printf("select your choice: \n");
+        scanf("%d",&choice2);
+      if(choice2==1 || choice2==2 ||  choice2==3 ||  choice2==4 ||  choice2==5 ||  choice2==6 ||  choice2==7 ||  choice2==8 ||  choice2==9 ||  choice2==10  ){
+        //   printf("You chose %s",flavour[choice2-1]);
+          printf("Select the size \n1.Small \n2.Large\n");
+           scanf("%d",&choice3);
+           if(choice3==1){
+               printf("You chose small size %s ice-cream \nIt costs Rs.%d\n",flavour[choice2-1],price_s[choice2-1]);
+           }
+           else if(choice3==2){
+                printf("You chose large size %s ice-cream \nIt costs Rs.%d\n",flavour[choice2-1],price_l[choice2-1]);
+           }
+           else{
+                printf("invalid option \nretry with valid option\n");
+                break;
+           }
 
-int main() {
-    // Create an array of struct icecream to store 10 flavors
-    struct icecream name[10] = {"Vanilla", "Strawberry", "Chocolate", "Pista", "Butterscotch","Mango", "Chocovanilla", "Coffee", "Caramel", "Brownie"};
 
-    // Display a welcome message and menu options
-    printf("Welcome To Ice Cream Parlour\n");
-    printf("1. Show menu\n");
-    printf("2. Exit\n");
+          printf("Confirm your order\n 1.confirm\n 2.cancel\n");
+          scanf("%d",&confirm);
+          if(confirm==1){
+              printf("your order confirmed\n Thank you visit us again\n");
+                num=0;
 
-    // Declare variables to store user's choices
-    int choice, choice2, choice3;
+          }
+          else{
+              printf("As you cancelled, Do u want select another flavour\n");
+              printf("1.Yes\n2.exit\n");
+              int reorder;
+              scanf("%d",&reorder);
+              if(reorder==1){
 
-    // Get user's choice
-    scanf("%d", &choice);
+              }
+              else{
+                  num=0;
+                  printf("Thank you visit us again\n");
+              }
+          }
+      }
+      else{
+          printf("invalid option \nretry with valid option\n\n");
+      }
+      break;
+      case 2:
+      printf("Thank u visit us again\n");
+      num=0;
+      break;
+      default:
+      printf("invalid option");
+       num=0;
+      break;
 
-    // Check user's choice
-    if (choice == 1) {
-        printf("We have 10 flavors for you\n");
-        
-        // Loop through the flavors and print them along with their index
-        for (int i = 0; i < 10; i++) {
-            printf("%d. %s\n", i + 1, name[i].flavour);
-        }
-        
-        // Prompt the user to choose a flavor
-        printf("Choose a flavor (1-10): ");
-        scanf("%d", &choice2);
 
-        // Check if the chosen flavor is valid
-        if (choice2 >= 1 && choice2 <= 10) {
-            printf("You chose %s\n", name[choice2 - 1].flavour);
+  }
 
-            // Prompt the user to exit
-            printf("Press 11 to exit\n");
-            scanf("%d", &choice3);
-            if (choice3 == 11) {
-                printf("Thank you, visit us again\n");
-            }
-        } else {
-            printf("Invalid flavor choice\n");
-        }
-    } else if (choice == 2) {
-        // User chose to exit directly
-        printf("Thank you, visit us again\n");
-    } else {
-        // Invalid choice
-        printf("Invalid choice\n");
-    }
+//   printf("%d",choice);
 
-    return 0;
+  }
 }
+
+
+int main() 
+{
+
+    // printf("%s\n",flavour[1]);
+
+  printf("Welcome To Ice Cream Parlour\n");
+  printf("1.Show menu\n");
+  printf("2.exit\n");
+  scanf("%d",&choice);
+  if(choice==1){
+      select();
+  }
+  else{
+      printf("Thank you visit us again\n");
+  }
+
+  return 0;
+}
+ 
